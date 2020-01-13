96be6cac85683be7db74291bf2f42e134dc0b088
Elizar Pepino <jupenz@gmail.com>
2020-01-13 13:00:36 +0800

    add trail

--- Character.gd
+++ Character.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-const DAMP = 25.0
+const DAMP = 10.0
 const MASS = 100.0
 const GRAVITY = 200.0
 const JUMP_SPEED = 1000.0
@@ -10,6 +10,11 @@ const MAX_WALK_SPEED = 400.0
 
 var velocity = Vector2()
 var is_jumping = false
+var trail: Particles2D
+
+func _ready():
+	trail = get_child(0)
+	
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY + MASS
@@ -21,6 +26,10 @@ func _physics_process(delta):
 		is_jumping = false
 	
 	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	if direction != 0 or !is_on_floor():
+		trail.emitting = true
+	else:
+		trail.emitting = false
 
 	# check if we're not moving left or right and if we are on the floor,
 	# then we decelerate
@@ -36,7 +45,7 @@ func _physics_process(delta):
 	
 	# add extra jump height if user is long-pressing the button
 	if is_jumping and Input.is_action_pressed("jump"):
-		velocity.y -= GRAVITY * 0.2
+		velocity.y -= GRAVITY * 0.3
 	
 	# add max walk speed
 	velocity.x = clamp(velocity.x, -MAX_WALK_SPEED, MAX_WALK_SPEED)
